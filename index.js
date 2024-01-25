4103db5dab323f86899c244595a65548b189c70d
Stefany Peixoto <Stefany Peixoto>
2024-01-24 23:20:37 -0700

    Creating boxes

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 const express = require('express'); // express makes APIs - connect frontend to database
 
 const app = express(); // Create an express application
+const Redis = require('redis'); // import the redis class from the library
+const bodyParser =require('body-parser');
 
-app.listen(3000); // Listen from web requests from the frontend and don't stop
+const redisClient = Redis.createClient({
+    url:`redis://localhost:6379`
+});
 
-const boxes = [
-    {boxId:1},
-    {boxId:2},
-    {boxId:3},
-    {boxId:4}
-];
+//app.listen(3000); // Listen from web requests from the frontend and don't stop
+const port =3000; // port number 
+app.use(bodyParser.json()); // makes body
+
+app.listen(port,()=>{
+    redisClient.connect(); // connect to the database!!!!!
+    console.log(`Listening on port: ${port}`); // template literal
+}); // listen for web request from the frontend and dont stop
+
+//http://localhost:3000/boxes
+
+app.post('/boxes',async (req,res)=>{ // async means we will await promises
+    const newBox = req.body; // now we have a box 
+    newBox.id= parseInt(await redisClient.json.arrLen('boxes','$'))+1; // the user shouldn't choose the ID
+    await redisClient.json.arrAppend('boxes','$',newBox);
+    res.json(newBox); // respond with a new box 
+})
+
 // 1- URL
 // 2- a function to return boxes 
 // req= the request from the browser
 // res= the response to the browser
-app.get('/boxes', (req, res)=>{
-    // send boxes to th browser
-    res.send(JSON.stringify(boxes)); // convert boxes to a string
+app.get('/boxes',async (req, res)=>{
+    let boxes = await redisClient.json.get('boxes',{path:'$'});
+    // send boxes to the browser
+    res.json(boxes); // convert boxes to a string
 
 }); // Return boxes to the user
 
