343dd431975059c8b91b4ec6fcbf81d293d7a4ea
Stefany Peixoto <Stefany Peixoto>
2024-04-01 10:52:13 -0600

    Orders lambda

--- lambda.js
+++ lambda.js
@@ -1,7 +1,46 @@
 
+const redis = require('redis');// import the redis class from the library
+
+const redisHost = process.env.REDIS_HOST;
+const redisPort = process.env.REDIS_PORT;
+
+const redisClient = redis.createClient({
+  host: redisHost, // Redis host
+  port: redisPort,        // Redis port
+});
+
 exports.handler = (event, context) => {
+  event.redisClient = redisClient;
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message:"Lambda Works!", event, context })
   }
 };
+
+exports.postOrders = async (event, context) => {
+
+	let order = req.body;
+    let responseStatus = 200;
+    const contentType = req.get('Content-Type');
+    console.log('Content-Type:', contentType); 
+	
+	if (responseStatus === 200) {
+	    try {
+	    // addOrder function to handle order creation in the database
+	    await addOrder ({ redisClient, order });
+	    } catch (error) {
+	    console.error(error);
+	    res.status(500).send("Internal Server Error");
+	    return;
+    }
+  } else {
+      res.status(resposeStatus);
+      res.send(
+      `Missing one of the following fields: ${
+      order.productQuantity ? "" : "productQuantity"
+      } ${order.ShippingAddress ? "" : "ShippingAddress"}`
+      );
+  }
+  res.status(responseStatus).send({message:"you are cool"});
+};  
